353408d1f8be3c2ddbca24807c25b41d18214eb7
mrfrolk <ekaterita20@gmail.com>
2025-04-22 12:38:22 +0500

    init

---
+++ lib/core/routing/router_config.g.dart
@@ -0,0 +1,28 @@
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'router_config.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$appRouterHash() => r'436f53c59eea24b0c7ff870ad3c6a206ce952ce9';
+
+/// See also [appRouter].
+@ProviderFor(appRouter)
+final appRouterProvider = AutoDisposeProvider<GoRouter>.internal(
+  appRouter,
+  name: r'appRouterProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$appRouterHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef AppRouterRef = AutoDisposeProviderRef<GoRouter>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
+  
